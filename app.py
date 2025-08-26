285093992f7f2e91c56f9a91b106425cd84788ca
caac95 <caac95pr@gmail.com>
2025-08-26 15:15:14 -0500

    implementacion manejo de errores al cargar datos

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df=pd.read_csv("../vehicles_us.csv")
+try:
+    df = pd.read_csv('vehiculo.csv', sep=',', encoding='utf-8', header=0)
+    print(df.head())
+except FileNotFoundError:
+    print("No se encontró el archivo 'vehiculo.csv'. Verifica que esté en la carpeta correcta.")
+except pd.errors.ParserError as e:
+    print("Error al analizar el archivo CSV:", e)
+except Exception as e:
+    print("Error inesperado:", e)
+
 
 if st.checkbox('Mostrar histograma'):
     st.write('Creación de un histograma para el odómetro de los vehículos')
